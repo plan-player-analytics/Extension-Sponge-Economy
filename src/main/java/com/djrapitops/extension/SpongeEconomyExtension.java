df3ee1d3b7b9ec416be56ffd1cc6650bd4ebf640
Risto Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2021-03-05 13:56:29 +0200

    Changed repository

--- src/main/java/com/djrapitops/extension/SpongeEconomyExtension.java
+++ src/main/java/com/djrapitops/extension/SpongeEconomyExtension.java
@@ -1,5 +1,5 @@
 /*
-    Copyright(c) 2019 Risto Lahtela (Rsl1122)
+    Copyright(c) 2019 Risto Lahtela (AuroraLS3)
 
     The MIT License(MIT)
 
@@ -40,7 +40,7 @@ import java.util.UUID;
  * <p>
  * Adapted from PluginData implementation by BrainStone.
  *
- * @author Rsl1122
+ * @author AuroraLS3
  */
 @PluginInfo(name = "Sponge Economy", iconName = "money-bill-wave", iconFamily = Family.SOLID, color = Color.AMBER)
 public class SpongeEconomyExtension implements DataExtension {
